9bcac508a2afab8274250d629569e7b4a89033f1
Eric Hosick <erichosick@gmail.com>
2016-02-20 00:01:48 -0800

    Refactoring.

--- dist/restoff.js
+++ dist/restoff.js
@@ -16,40 +16,47 @@ if (typeof module !== 'undefined' && module.exports) {
 	root.restlib = restlib;
 }
 function restoff() {
-	return Object.create(RestOff.prototype);
+	var that = Object.create(RestOff.prototype);
+	that._isOnline = false;
+	return that;
 }
 
 function RestOff() {}
 RestOff.prototype = Object.create(Object.prototype, {
 	isOnline: {
+		get: function() { return this._isOnline; },
+		set: function(value) { this._isOnline = value; }
+	},
+	getRequest: {
 		get: function() {
-			return true;
+			return window.XMLHttpRequest ?
+				new XMLHttpRequest() : // Mozilla, Safari, ...
+				new ActiveXObject("Microsoft.XMLHTTP"); // IE 8 and older
 		}
 	}
 });
 RestOff.prototype.get = function(uri) {
+	var that = this;
 	var promise = new Promise(function(resolve, reject) {
-		var _xhr;
-		if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-			_xhr = new XMLHttpRequest();
-		} else if (window.ActiveXObject) { // IE 8 and older
-			_xhr = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-
-		_xhr.open("GET", uri, true); // true = asynchronous. synchronous on main thread is deprecated
-		_xhr.onload = function(){
-			if(_xhr.readyState == 4) {
-				if(_xhr.status == 200) {
-					resolve(JSON.parse(_xhr.response)); // success: resolve promise
+		var request = that.getRequest;
+		request.open("GET", uri, true); // true: asynchronous
+		request.onload = function(){
+			if(request.readyState == 4) { // 4: Request finished and response is ready
+				if(request.status == 200) {
+					that.isOnline = true;
+					resolve(JSON.parse(request.response));
+					// TODO: Check for non-json result
 				} else {
-					reject(new Error("something went wrong")); // error: reject promise
+					reject(new Error("something went wrong"));
 				}
+			} else {
+				console.log("Warning: unexpected readyState of " + request.readyState + ".");
 			}
 		}
-		_xhr.onerror = function(error){
+		request.onerror = function(error){
 			reject(error);
 		}
-		_xhr.send();
+		request.send();
 	});
 	return promise;
 }
--- testsweb/tests/restoff.js
+++ testsweb/tests/restoff.js
@@ -10,10 +10,10 @@ describe ("running web specific tests", function() {
 
 	it("should be offline initially", function() {
 		var roff = restoff();
-		expect(roff.isOnline).to.be.true;
+		expect(roff.isOnline).to.be.false;
 	});
 	
-	it("should access an endpoint", function() {
+	it("should access a valid endpoint while connected", function() {
 		var roff = restoff();
 		var user01 = {
 			"id": "aedfa7a4-d748-11e5-b5d2-0a1d41d68578",
@@ -24,7 +24,9 @@ describe ("running web specific tests", function() {
 		return roff.get("http://test.development.com:4050/testsweb/testdata/user01.json")
 		.then(function(result){
 			expect(result).to.deep.equals(user01);
+			expect(roff.isOnline).to.be.true;
 		});
 	});
+
 });
 
